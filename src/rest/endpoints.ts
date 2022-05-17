8f0de5fd935a4548849d80e76cdaf876c8df2b15
Alistair Smith <alistairsmith01@gmail.com>
2022-05-17 19:06:12 +0100

    feat: moar work on endpoints

--- src/rest/endpoints.ts
+++ src/rest/endpoints.ts
@@ -1,35 +1,4 @@
-import {Method, Responses} from './types';
-
-export type Endpoint<
-	M extends Method,
-	Path extends string,
-	Res,
-	Body = never,
-> = {
-	method: M;
-	path: Path;
-	res: Res;
-	body: Body;
-};
-
-export type Endpoints =
-	| Endpoint<'GET', '/v1/pipe/rooms', Responses.PIPE.GET_ROOMS>
-	| Endpoint<
-			'POST',
-			'/v1/pipe/rooms',
-			Responses.PIPE.CREATE_ROOM,
-			{
-				/**
-				 * The name of the room
-				 */
-				name: string;
-
-				/**
-				 * Any information attatched to the room
-				 */
-				metadata: unknown;
-			}
-	  >;
+import {Method, API} from './types';
 
 export type SuccessfulAPIResposne<T> = {
 	success: true;
@@ -42,4 +11,64 @@ export type ErroredAPIResponse = {
 		code: string;
 		message: string;
 	};
+};
+
+export type APIResponse<T> = SuccessfulAPIResposne<T> | ErroredAPIResponse;
+
+export type Endpoint<
+	M extends Method,
+	Path extends string,
+	Res,
+	Body = never,
+> = {
+	method: M;
+	path: Path;
+	res: APIResponse<Res>;
+	body: Body;
+};
+
+export type Endpoints =
+	| Endpoint<'GET', '/v1/pipe/rooms', API.Pipe.GET_ROOMS>
+	| Endpoint<
+			'POST',
+			'/v1/pipe/rooms',
+			API.Pipe.CREATE_ROOM,
+			{
+				/**
+				 * The name of the room
+				 */
+				name: string;
+
+				/**
+				 * Any information attatched to the room
+				 */
+				metadata: API.Pipe.RoomMetadata;
+			}
+	  >
+	| Endpoint<
+			'POST',
+			'/v1/pipe/rooms/:room_id/join-token',
+			API.Pipe.CREATE_JOIN_TOKEN,
+			{
+				/**
+				 * The user id of the user to join into this room
+				 * This should be the user id on YOUR systems
+				 */
+				user_id: string | number;
+
+				/**
+				 * Any information attatched to the room
+				 */
+				metadata: API.Pipe.RoomMetadata;
+			}
+	  >
+	| Endpoint<'GET', '/v1/teams/:team_id/members/@me', API.Teams.Member>
+	| Endpoint<
+			'GET',
+			'/v1/ignite/deployments/:deployment_id/containers',
+			API.Ignite.Container
+	  >;
+
+export type EndpointMap = {
+	[Path in Endpoints['path']]: Extract<Endpoints, {path: Path}>;
 };
--- src/rest/types.ts
+++ src/rest/types.ts
@@ -1,10 +1,77 @@
 export type Method = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
 
-export namespace Responses {
-	export namespace PIPE {
+export const ID_PREFIXES = [
+	{
+		prefix: 'user',
+		description: 'Users',
+	},
+	{
+		prefix: 'team',
+		description: 'Teams',
+	},
+	{
+		prefix: 'tm',
+		description: 'Team Members',
+	},
+	{
+		prefix: 'role',
+		description: 'Roles',
+	},
+	{
+		prefix: 'ti',
+		description: 'Team Invite',
+	},
+	{
+		prefix: 'sk',
+		description: 'Secret Key',
+	},
+	{
+		prefix: 'pat',
+		description: 'Team members personal access token',
+	},
+	{
+		prefix: 'container',
+		description: 'Ignite container',
+	},
+	{
+		prefix: 'room',
+		description: 'Pipe room',
+	},
+	{
+		prefix: 'deployment',
+		description: 'Ignite deployment',
+	},
+] as const;
+
+export type IdPrefixes = typeof ID_PREFIXES[number]['prefix'];
+export type Id<T extends IdPrefixes> = `${T}_${string}`;
+export type AnyId = Id<IdPrefixes>;
+
+export function validateId<T extends IdPrefixes>(
+	maybeId: string,
+	prefix: T,
+): maybeId is Id<T> {
+	return maybeId.startsWith(prefix);
+}
+
+export namespace API {
+	export namespace Pipe {
+		export type RoomMetadata = unknown;
+
 		export interface Room {
-			id: string;
+			/**
+			 * The ID of this room
+			 */
+			id: Id<'room'>;
+
+			/**
+			 * The name of this room
+			 */
 			name: string;
+
+			/**
+			 * The unix timestamp of when this room was created
+			 */
 			created_at: number;
 		}
 
@@ -14,6 +81,57 @@ export namespace Responses {
 
 		export interface CREATE_ROOM {
 			room: Room;
+		}
+
+		export interface CREATE_JOIN_TOKEN {
+			join_token: string;
+		}
+	}
+
+	export namespace Teams {
+		export interface Member {
+			/**
+			 * The ID of the user
+			 */
+			id: Id<'tm'>;
+			/**
+			 * The personal access token of this user.
+			 * Most characters will be hidden if this is NOT the first time you have seen this token.
+			 */
+			pat: string;
+			/**
+			 * The role of this user in the team
+			 */
+			role: MemberRole;
+		}
+
+		export interface MemberRole {
+			/**
+			 * The ID of the role
+			 */
+			id: Id<'role'>;
+			/**
+			 * The name of the role
+			 */
+			name: string;
+			/**
+			 * The flags for this role
+			 */
+			flag: number;
+		}
+	}
+
+	export namespace Ignite {
+		export interface Container {
+			id: Id<'container'>;
+			created_at: string;
+			type: number;
+			deployment_id: string;
+			state: number;
+		}
+
+		export interface GET_DEPLOYMENT_CONTAINERS {
+			containers: Container[];
 		}
 	}
 }
