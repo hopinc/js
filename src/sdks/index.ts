f8d7e20d7323871f3fe42b6cc414bff64e70d1d6
Alistair Smith <hi@alistair.sh>
2022-07-30 10:37:43 +0200

    fix: export all sdks

--- src/sdks/index.ts
+++ src/sdks/index.ts
@@ -2,3 +2,5 @@ export {Ignite} from './ignite';
 export {Pipe} from './pipe';
 export {Projects} from './projects';
 export {User} from './user';
+export {Registry} from './registry';
+export {Channels} from './channels';
