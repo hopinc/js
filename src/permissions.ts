affd4df227bf2740bcada3b5071370ad39d7e74d
Cody Miller <hello@looskie.com>
2022-08-01 23:19:52 -0400

    chore:cleanup

--- src/permissions.ts
+++ src/permissions.ts
@@ -191,7 +191,7 @@ export const permissions = {
 	},
 
 	test(value: bigint, flag: bigint) {
-		return (BigInt(value) & BigInt(flag)) === BigInt(flag);
+		return BigInt(value) & BigInt(flag);
 	},
 
 	subtract(value: bigint, flag: bigint) {
