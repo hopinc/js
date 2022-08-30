dbe92a07527ffeba07e1cffd9605e629861e7455
Alistair Smith <hi@alistair.sh>
2022-08-30 20:11:26 +0200

    fix: cleanup imports

--- src/rest/types/api.ts
+++ src/rest/types/api.ts
@@ -1,8 +1,6 @@
-import * as Channels from './channels.js';
-import * as Registry from './registry.js';
-import * as Ignite from './ignite.js';
-import * as Pipe from './pipe.js';
-import * as Projects from './projects.js';
-import * as Users from './users.js';
-
-export {Pipe, Users, Projects, Ignite, Channels, Registry};
+export * as Channels from './channels.js';
+export * as Registry from './registry.js';
+export * as Ignite from './ignite.js';
+export * as Pipe from './pipe.js';
+export * as Projects from './projects.js';
+export * as Users from './users.js';
