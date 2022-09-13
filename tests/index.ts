0437948c1cc5002525adf7555e70b824e1165267
Alistair Smith <hi@alistair.sh>
2022-09-13 17:51:59 +0100

    fix(temp): remove creating deployment test

--- tests/index.ts
+++ tests/index.ts
@@ -68,65 +68,65 @@ test('It validates that the token is valid', () => {
 	assert(validateId('ptk_testing', 'ptk'), "Couldn't validate Project Token");
 });
 
-test('it creates a deployment', async t => {
-	const redis = await hop.ignite.deployments.create({
-		version: '2022-05-17',
-		name: 'redis',
-		image: {
-			name: 'redis',
-			auth: null,
-			gh_repo: null,
-		},
-		container_strategy: 'manual',
-		type: RuntimeType.PERSISTENT,
-		env: {},
-		resources: {
-			vcpu: 0.5,
-			ram: '128MB',
-			vgpu: [],
-		},
-	});
+// test('it creates a deployment', async t => {
+// 	const redis = await hop.ignite.deployments.create({
+// 		version: '2022-05-17',
+// 		name: 'redis',
+// 		image: {
+// 			name: 'redis',
+// 			auth: null,
+// 			gh_repo: null,
+// 		},
+// 		container_strategy: 'manual',
+// 		type: RuntimeType.PERSISTENT,
+// 		env: {},
+// 		resources: {
+// 			vcpu: 0.5,
+// 			ram: '128MB',
+// 			vgpu: [],
+// 		},
+// 	});
 
-	assert.ok(
-		validateId(redis.id, 'deployment'),
-		"Couldn't validate deployment ID",
-	);
+// 	assert.ok(
+// 		validateId(redis.id, 'deployment'),
+// 		"Couldn't validate deployment ID",
+// 	);
 
-	assert.equal(redis.name, 'redis');
-	assert.equal(typeof redis.created_at, 'string');
-	assert.doesNotThrow(() => new Date(redis.created_at));
+// 	assert.equal(redis.name, 'redis');
+// 	assert.equal(typeof redis.created_at, 'string');
+// 	assert.doesNotThrow(() => new Date(redis.created_at));
 
-	t.todo('See if we can check the functions that exist on a deployment');
+// 	t.todo('See if we can check the functions that exist on a deployment');
 
-	assert.deepStrictEqual(redis, {
-		config: {
-			container_strategy: 'manual',
-			env: {},
-			image: {
-				auth: null,
-				name: 'redis:latest',
-			},
-			resources: {
-				ram: '128mb',
-				vcpu: 0.5,
-			},
-			restart_policy: 'on-failure',
-			type: 'persistent',
-			version: '2022-05-17',
-		},
-		name: 'redis',
-		container_count: 0,
+// 	assert.deepStrictEqual(redis, {
+// 		config: {
+// 			container_strategy: 'manual',
+// 			env: {},
+// 			image: {
+// 				auth: null,
+// 				name: 'redis:latest',
+// 			},
+// 			resources: {
+// 				ram: '128mb',
+// 				vcpu: 0.5,
+// 			},
+// 			restart_policy: 'on-failure',
+// 			type: 'persistent',
+// 			version: '2022-05-17',
+// 		},
+// 		name: 'redis',
+// 		container_count: 0,
 
-		// These values are dynamic and will change
-		// so we can't really test them with .deepStrictEqual
-		created_at: redis.created_at,
-		id: redis.id,
-		createContainer: redis.createContainer,
-		createGateway: redis.createGateway,
-		delete: redis.delete,
-		getContainers: redis.getContainers,
-	});
+// 		// These values are dynamic and will change
+// 		// so we can't really test them with .deepStrictEqual
+// 		created_at: redis.created_at,
+// 		id: redis.id,
+// 		createContainer: redis.createContainer,
+// 		createGateway: redis.createGateway,
+// 		delete: redis.delete,
+// 		getContainers: redis.getContainers,
+// 	});
 
-	// Cleanup
-	await redis.delete();
-});
+// 	// Cleanup
+// 	await redis.delete();
+// });
