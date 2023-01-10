14fa8880a3443c71dece45cacc6bbe0ea8f883e2
Alistair Smith <hi@alistair.sh>
2023-01-10 16:26:23 +0000

    fix: patch deployment shld be partial

--- src/sdks/ignite.ts
+++ src/sdks/ignite.ts
@@ -337,7 +337,10 @@ export const ignite = sdk(client => {
 				});
 			},
 
-			async update(deploymentId: Id<'deployment'>, config: DeploymentConfig) {
+			async update(
+				deploymentId: Id<'deployment'>,
+				config: Partial<DeploymentConfig>,
+			) {
 				const {deployment} = await client.patch(
 					'/v1/ignite/deployments/:deployment_id',
 					config,
