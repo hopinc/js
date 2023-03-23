5fc83888cad9259c60d27c2cba3428a6f5989ee9
Alistair Smith <hi@alistair.sh>
2023-03-23 11:04:33 +0000

    feat: init typed channels experiment

---
+++ src/utils/typed-channels/index.ts
@@ -0,0 +1,42 @@
+import type {z} from 'zod';
+import type {Hop} from '../../hop.ts';
+
+export type AnyEventsDefinition = Record<string, unknown>;
+
+export function createTypedChannelsEmitter<
+	T extends AnyEventsDefinition = AnyEventsDefinition,
+>(
+	hop: Hop,
+	schemas?: {
+		[Key in keyof T]: z.Schema<T[Key]>;
+	},
+) {
+	return {
+		selectChannel: (channel: string) => ({
+			publish: async <K extends keyof T>(
+				event: Extract<K, string>,
+				data: T[K],
+			) => {
+				const parsed = (await schemas?.[event].parseAsync(data)) ?? data;
+
+				await hop.channels.publishMessage(channel, event, parsed);
+			},
+		}),
+	};
+}
+
+declare const hop: Hop;
+
+type PublishableEvents = {
+	CREATE_MESSAGE: {
+		content: string;
+	};
+};
+
+const client = createTypedChannelsEmitter<PublishableEvents>(hop);
+
+const messages = client.selectChannel('messages');
+
+await messages.publish('CREATE_MESSAGE', {
+	content: '',
+});
